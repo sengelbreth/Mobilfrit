353239a65960d5f64c7e3dab1af903979f435b58
sengelbreth <sophie.engel1202@gmail.com>
2019-10-31 13:55:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="Da">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mobilfrit eventyr</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <div id="button-con">
+            <button id="login">Log på</button>
+            <button id="tilmed">Tilmed dig</button>
+        </div>
+
+        <img src="9bedaa54c3c74c82bd95aed2d7802cec.jpg">
+        <h1 id="splash">Velkommen til Mobilfrit-eventyr</h1>
+    </div>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
